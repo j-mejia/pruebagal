55450e344d63b616a95ee39cd09e55263f928741
Jonathan <jariel2313@hotmail.com>
2019-07-10 17:00:35 -0600

    primer commit
    commit1

---
+++ controllers/examenform.control.php
@@ -0,0 +1,135 @@
+<?php
+  require_once "models/examendata.model.php";
+  function run()
+  {
+      $estadoJuguetes = obtenerEstados();
+      $selectedEst = 'ACT';
+      $mode = "";
+      $errores=array();
+      $hasError = false;
+      $modeDesc = array(
+        "DSP" => "JUGUETE ",
+        "INS" => "Creando Nuevo Juguete",
+        "UPD" => "Actualizando Juguete ",
+        "DEL" => "Eliminando Juguete "
+      );
+      $viewData = array();
+      $viewData["showIdJuguetes"] = true;
+      $viewData["showBtnConfirmar"] = true;
+      $viewData["readonly"] = '';
+      $viewData["selectDisable"] = '';
+
+      if (isset($_POST["xcfrt"]) && isset($_SESSION["xcfrt"]) &&  $_SESSION["xcfrt"] !== $_POST["xcfrt"]) {
+          redirectWithMessage(
+              "Petición Solicitada no es Válida",
+              "index.php?page=examenlist"
+          );
+          die();
+      }
+      $viewData["xcfrt"] = $_SESSION["xcfrt"];
+      if (isset($_POST["btnDsp"])) {
+          $mode = "DSP";
+          $moda = obtenerJuguetePorId($_POST["idjuguetes"]);
+          $selectedEst=$moda["estadojuguete"];
+          $viewData["showBtnConfirmar"] = false;
+          $viewData["readonly"] = 'readonly';
+          $viewData["selectDisable"] = 'disabled';
+          mergeFullArrayTo($moda, $viewData);
+          $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+      }
+      if (isset($_POST["btnUpd"])) {
+          $mode = "UPD";
+          //Vamos A Cargar los datos
+          $moda = obtenerJuguetePorId($_POST["idjuguetes"]);
+          $selectedEst=$moda["estadojuguete"];
+          mergeFullArrayTo($moda, $viewData);
+          $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+      }
+      if (isset($_POST["btnDel"])) {
+          $mode = "DEL";
+          //Vamos A Cargar los datos
+          $moda = obtenerJuguetePorId($_POST["idjuguetes"]);
+          $selectedEst=$moda["estadojuguete"];
+          $viewData["readonly"] = 'readonly';
+          $viewData["selectDisable"] = 'disabled';
+          mergeFullArrayTo($moda, $viewData);
+          $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+      }
+      if (isset($_POST["btnIns"])) {
+          $mode = "INS";
+          //Vamos A Cargar los datos
+          $viewData["modeDsc"] = $modeDesc[$mode];
+           $viewData["showIdJuguetes"]  = false;
+      }
+      // if ($mode == "") {
+      //     print_r($_POST);
+      //     die();
+      // }
+      if (isset($_POST["btnConfirmar"])) {
+          $mode = $_POST["mode"];
+          $selectedEst = $_POST["estjuguete"];
+           mergeFullArrayTo($_POST, $viewData);
+          switch($mode)
+          {
+          case 'INS':
+              $viewData["showIdJuguetes"] = false;
+              $viewData["modeDsc"] = $modeDesc[$mode];
+              //validaciones
+              if (floatval($viewData["precjuguete"]) <= 0) {
+                  $errores[] = "El precio de juguete no puede ser 0";
+                  $hasError = true;
+              }
+              if (!$hasError && agregarNuevoJuguete(
+                  $viewData["dscjuguete"],
+                  $viewData["precjuguete"],
+                  $viewData["estjuguete"]
+              )
+              ) {
+                  redirectWithMessage(
+                      "Juguete Guardada Exitosamente",
+                      "index.php?page=examenlist"
+                  );
+                  die();
+              }
+              break;
+          case 'UPD':
+              $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+              if (modificarJuguete(
+                  $viewData["dscjuguete"],
+                  $viewData["precjuguete"],
+                  $viewData["estjuguete"],
+                  $viewData["idjuguetes"]
+              )
+              ) {
+                  redirectWithMessage(
+                      "Juguete Actualizado Exitosamente",
+                      "index.php?page=examenlist"
+                  );
+                  die();
+              }
+              break;
+          case 'DEL':
+              $viewData["modeDsc"] = $modeDesc[$mode] . $viewData["dscjuguete"];
+              $viewData["readonly"] = 'readonly';
+              $viewData["selectDisable"] = 'disabled';
+              if (eliminarJuguete(
+                  $viewData["idjuguetes"]
+              )
+              ) {
+                  redirectWithMessage(
+                      "Juguete Eliminado Exitosamente",
+                      "index.php?page=examenlist"
+                  );
+                  die();
+              }
+              break;
+          }
+      }
+      $viewData["mode"] = $mode;
+      $viewData["estadoJuguetes"] = addSelectedCmbArray($estadoJuguetes, 'cod', $selectedEst);
+      $viewData["hasErrors"] = $hasError;
+      $viewData["errores"] = $errores;
+      renderizar("examenform", $viewData);
+  }
+  run();
+?>
---
+++ views/examenform.view.tpl
@@ -0,0 +1,70 @@
+<h1>{{modeDsc}}</h1>
+<section class="row">
+<form action="index.php?page=examenform" method="post" class="col-8 col-offset-2">
+  {{if hasErrors}}
+    <section class="row">
+      <ul class="error">
+        {{foreach errores}}
+          <li>{{this}}</li>
+        {{endfor errores}}
+      </ul>
+    </section>
+  {{endif hasErrors}}
+  <input type="hidden" name="mode" value="{{mode}}"/>
+  <input type="hidden" name="xcfrt" value="{{xcfrt}}" />
+  <input type="hidden" name="btnConfirmar" value="Confirmar" />
+  {{if showIdJuguetes}}
+  <fieldset class="row">
+    <label class="col-5" for="idjuguetes">Código de Juguete</label>
+    <input type="text" name="idjuguetes" id="idjuguetes" readonly value="{{idjuguetes}}" class="col-7" />
+  </fieldset>
+  {{endif showIdJuguetes}}
+  <fieldset class="row">
+    <label class="col-5" for="dscjuguete">Nombre: </label>
+    <input type="text" name="dscjuguete" id="dscjuguete" {{readonly}} value="{{nomjuguete}}" class="col-7" />
+  </fieldset>
+  <fieldset class="row">
+    <label class="col-5" for="precjuguete">Precio de Venta</label>
+    <input type="text" name="precjuguete" id="precjuguete" {{readonly}} value="{{preciojuguete}}" class="col-7" />
+  </fieldset>
+  <fieldset class="row">
+    <label class="col-5" for="estjuguete">Estado</label>
+    <select name="estjuguete" id="estjuguete" class="col-7" {{selectDisable}} {{readonly}} >
+      {{foreach estadoJuguetes}}
+        <option value="{{cod}}" {{selected}}>{{dsc}}</option>
+      {{endfor estadoJuguetes}}
+    </select>
+  </fieldset>
+  <fieldset class="row">
+    <div class="right">
+      {{if showBtnConfirmar}}
+      <button type="button" id="btnConfirmar" >Confirmar</button>
+      &nbsp;
+      {{endif showBtnConfirmar}}
+      <button type="button" id="btnCancelar">Cancelar</button>
+    </div>
+  </fieldset>
+  <!--
+   <td>{{idmoda}}</td>
+    <td>{{dscmoda}}</td>
+    <td>{{prcmoda}}</td>
+    <td>{{ivamoda}}</td>
+    <td>{{estmoda}}</td>
+   -->
+</form>
+</section>
+<script>
+  $().ready(function(){
+    $("#btnCancelar").click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      location.assign("index.php?page=examenlist");
+    });
+    $("#btnConfirmar").click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      /*Aqui deberia hacer validación de datos*/
+      document.forms[0].submit();
+    });
+  });
+</script>
